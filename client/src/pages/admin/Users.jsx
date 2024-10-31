bc443a6687279782722bb4adf6e3ad811e910170
NJ Veneracion <enjayveneracion@gmail.com>
2024-10-31 12:19:22 +0800

    added fields on user model

--- client/src/pages/admin/Users.jsx
+++ client/src/pages/admin/Users.jsx
@@ -20,28 +20,35 @@ const Users = () => {
     fetchUsers();
   }, []);
   return (
-    <section className="overflow-x-auto p-10">
+    <section className="">
       <h1 className="font-bold text-3xl mb-5">Users</h1>
       <div className="border p-10">
         <table className="table">
-          {/* head */}
           <thead>
             <tr>
               <th></th>
+              <th>Fullname</th>
               <th>Username</th>
               <th>Email</th>
               <th>Password</th>
               <th>Role</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, index) => (
               <tr key={user._id}>
                 <th>{index + 1}</th>
+                <td>{user.personal_information[0]?.fullname}</td>
                 <td>{user.username}</td>
                 <td>{user.email}</td>
                 <td>{user.password}</td>
                 <td>{user.role}</td>
+                <td>
+                  <button className="btn btn-info btn-sm mx-1">View</button>
+                  <button className="btn btn-warning btn-sm mx-1">Edit</button>
+                  <button className="btn btn-error btn-sm mx-1">Delete</button>
+                </td>
               </tr>
             ))}
           </tbody>
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -7,6 +7,12 @@ const userSchema = new Schema(
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true, minlength: 8 },
     role: { type: String, enum: ["Admin", "Customer"], default: "Customer" },
+    personal_information: [
+      {
+        fullname: { type: String, required: true },
+        contact_number: { type: String, required: true },
+      },
+    ],
     createdAt: { type: Date, default: Date.now },
   },
   {
