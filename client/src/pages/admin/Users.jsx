9530dda13e9aa1c4da44c3a1bac8af8dddc7f9c5
NJ Veneracion <enjayveneracion@gmail.com>
2024-11-01 15:10:46 +0800

    branch experimenting

--- client/src/pages/admin/Users.jsx
+++ client/src/pages/admin/Users.jsx
@@ -34,12 +34,6 @@ const Users = () => {
               <th>Password</th>
               <th>Role</th>
               <th>Action</th>
-              <th>Fullname</th>
-              <th>Username</th>
-              <th>Email</th>
-              <th>Password</th>
-              <th>Role</th>
-              <th>Action</th>
             </tr>
           </thead>
           <tbody>
